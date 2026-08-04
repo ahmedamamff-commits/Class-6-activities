94489426f120af322684e7551cae69fbc783df00
ahmedamamff-commits <ahmedamamff@gmail.com>
2026-08-03 18:37:18 -0700

    Add files via upload

---
+++ main4.py
@@ -0,0 +1,5 @@
+number = int(input("Enter a number to be checked for even or odd status:"))
+if number%2 == 0:
+    print("The number inputted is an even number")
+else:
+    print("The number inputted is an odd number")
